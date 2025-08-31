e48fc02c37bba36ff17ccd9a4404ffdea7bc8b6f
Imtiyaz272 <syedaimtiyaz19@gmail.com>
2025-08-31 18:22:26 +0530

    mongoURL

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const mongoURL = "mongodb+srv://syedaimtiyaz19:Immi@clusterwp.cndxvuj.mongodb.net/mydb";
+export const mongoURL = "mongodb+srv://<user>:<password>@clusterwp.cndxvuj.mongodb.net/mydb";
 export const PORT = 5000;
